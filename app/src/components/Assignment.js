6327843fb6be74c0c87f1137de5749d1c1851be0
NanguRepo <nanguthenangu@gmail.com>
2022-03-28 00:56:22 +0200

    make date red if overdue

--- app/src/components/Assignment.js
+++ app/src/components/Assignment.js
@@ -4,18 +4,21 @@ import Linkify from 'react-linkify'
 
 export default function Assignment(props) {
     const [ opened, setOpened ] = useState(false);
+    function isBeforeToday() {
+        const today = new Date()
+        const date = new Date(props.content.date + ' ' + today.getFullYear())
+        return date < today
+    }
     return (
         <div className="w-full h-full">
             <Card shadow="sm" p="lg" className="bg-gray-100 dark:bg-gray-700 dark:text-white h-full">
-                    <div className="flex flex-col items-center justify-between mb-2">
+                    <div className="flex flex-col items-center justify-between mb-4">
                         <h1 className="text-2xl font-bold">{props.content.subject}</h1>
-                        <div className="flex flex-col items-center justify-between mb-2">
-                            <p className="text-gray-500 dark:text-gray-400">{props.content.date}</p>
-                            <p className="text-gray-600 dark:text-gray-300">{props.content.time}</p>
-                        </div>
+                        <p className={`text-gray-500 dark:text-gray-400 ${isBeforeToday() ? 'text-red-600 dark:text-red-400':null}`}>{props.content.date}</p>
+                        <p className="text-gray-600 dark:text-gray-300">{props.content.time}</p>
                     </div>
                 <p id="description" className={`text-md ${props.fullText ? null:'truncate'}`}><Linkify properties={{ target: '_blank', style: { color: '#0000FF' }}}>{props.content.description}</Linkify></p>
-                {props.fullText ? null:<Button onClick={() => {setOpened(true)}} variant="light" color="blue" fullWidth style={{ marginTop: 14 }} className="hover:bg-gray-300/50 dark:hover:bg-gray-800/50">
+                {props.fullText ? null:<Button onClick={() => {setOpened(true)}} variant="light" color="blue" fullWidth style={{ marginTop: 14 }} className="hover:bg-gray-300/50 dark:hover:bg-gray-800/50 dark:text-blue-400">
                     See more
                 </Button>}
             </Card>
