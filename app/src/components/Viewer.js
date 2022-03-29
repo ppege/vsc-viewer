4d10254c107f4226b918dcc586d7ba8835edfe15
NanguRepo <nanguthenangu@gmail.com>
2022-03-29 09:04:33 +0200

    general viewer upgrades

--- app/src/components/Viewer.js
+++ app/src/components/Viewer.js
@@ -7,16 +7,22 @@ import dayjs from 'dayjs'
 
 export default function Viewer(props) {
     const findStartOfWeek = (Date) => {
-        return dayjs(Date).startOf('week')
+        return dayjs(Date).startOf('week').add(1, 'day')
     }
     const dateToString = (dateString) => {
         return findStartOfWeek(dayjs(dateString)).format('YYYY-MM-DD')
     }
-    const backOneWeek = () => {
-        setDate(dateToString(dayjs(date).subtract(7, 'day')))
-    }
-    const forwardOneWeek = () => {
-        setDate(dateToString(dayjs(date).add(7, 'day')))
+    const travelOneWeek = (direction) => {
+        switch (direction) {
+            case 'back':
+                setDate(dateToString(dayjs(date).subtract(7, 'day')))
+                break;
+            case 'forward':
+                setDate(dateToString(dayjs(date).add(7, 'day')))
+                break;
+            default:
+                break;
+        }
     }
     const [ date, setDate ] = useState(dateToString(new Date()));
     const [ assignments, setAssignments ] = useState(new Map())
@@ -33,7 +39,7 @@ export default function Viewer(props) {
             setAssignments(<Assignment {...{subject: "Credentials not set", description: "Please enter your ViGGO credentials in the settings menu."}} key="Loading" fullText={true} />)
             return;
         }
-        setAssignments(<Assignment {...{subject: "Loading...", description: "Please wait a moment. This will never finish loading if you haven't set your credentials correctly in the settings."}} key="Loading" fullText={true} />)
+        setAssignments(<Assignment {...{subject: "Loading...", description: "Please wait a moment."}} key="Loading" fullText={true} />)
         axios.get(`https://api.nangurepo.com/v2/scrape?username=${props.settings.username}&password=${props.settings.password}&subdomain=${props.settings.subdomain}&date=${date}`)
         .then((response) => {
             if (!response.data.length) {
@@ -48,10 +54,10 @@ export default function Viewer(props) {
     }, [props.settings, date])
     return (
         <div className="flex flex-col">
-            <div className="bg-gray-100 dark:bg-gray-700 items-center justify-center text-center rounded w-fit h-fit flex flex-row">
-                <IconButton icon={<FaAngleLeft size="30" />} className="w-min h-min border-0" onClick={backOneWeek} />
+            <div className="bg-gray-100 dark:bg-gray-700 items-center justify-center text-center rounded w-full md:w-1/2 md:justify-between h-fit flex flex-row">
+                <IconButton icon={<FaAngleLeft size="30" />} className="w-min h-min border-0" onClick={() => travelOneWeek('back')} />
                 <h1 className="text-xl md:text-3xl font-mono mr-2 ml-2">{date}</h1>
-                <IconButton icon={<FaAngleRight size="30" />} className="w-min h-min border-0" onClick={forwardOneWeek} />
+                <IconButton icon={<FaAngleRight size="30" />} className="w-min h-min border-0" onClick={() => travelOneWeek('forward')} />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
                 {assignments}
